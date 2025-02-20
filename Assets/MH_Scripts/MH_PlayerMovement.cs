2616153caa15822ee6625c0798bc319138d8e406
markkuVuoksi <markku.hautamaki@vuoksi.fi>
2025-02-20 13:47:44 +0200

    Added Player movement and jumping

---
+++ Assets/MH_Scripts/MH_PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MH_PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpHeight = 2f;
+    public Camera playerCamera;
+    private CharacterController characterController;
+    private Vector3 playerVelocity;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        isGrounded = characterController.isGrounded;
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 forward = playerCamera.transform.forward;
+        Vector3 right = playerCamera.transform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 movement = (forward * moveVertical + right * moveHorizontal).normalized;
+        characterController.Move(movement * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+        }
+
+        playerVelocity.y += Physics.gravity.y * Time.deltaTime;
+        characterController.Move(playerVelocity * Time.deltaTime);
+    }
+}
